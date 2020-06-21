f17e4ca0959952eb836e93a931ecf9a71bd5aa06
feritydn <feritydn@gmail.com>
2020-06-21 16:33:26 -0300

    feature branchtan ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -7,6 +7,8 @@ public class FirstClass {
 		System.out.println("ilk commit from local repository");
 		System.out.println("ikinci commit from local repository");
 		
+		System.out.println("yeni branch olusturdum ve adi feature");
+		
 
 	}
 
